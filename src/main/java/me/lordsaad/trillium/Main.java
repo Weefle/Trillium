25649c983462f93cb3efbf3a1118ae8cb33beda8
LordSaad <saadodi44@gmail.com>
2015-01-30 20:30:36 +0200

    Added ServerListPing class.

--- src/main/java/me/lordsaad/trillium/Main.java
+++ src/main/java/me/lordsaad/trillium/Main.java
@@ -2,6 +2,7 @@ package me.lordsaad.trillium;
 
 import me.lordsaad.trillium.events.PlayerJoin;
 import me.lordsaad.trillium.events.PlayerLeave;
+import me.lordsaad.trillium.events.ServerListPing;
 import me.lordsaad.trillium.motd.CommandMotd;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,6 +18,7 @@ public class Main extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
         getServer().getPluginManager().registerEvents(new PlayerLeave(), this);
+        getServer().getPluginManager().registerEvents(new ServerListPing(), this);
 
         getCommand("trillium").setExecutor(new CommandTrillium());
         getCommand("motd").setExecutor(new CommandMotd());
---
+++ src/main/java/me/lordsaad/trillium/events/ServerListPing.java
@@ -0,0 +1,20 @@
+package me.lordsaad.trillium.events;
+
+import me.lordsaad.trillium.Main;
+import net.md_5.bungee.api.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
+
+/**
+ * Created by saad on 30-Jan-15.
+ */
+public class ServerListPing implements Listener {
+
+    @EventHandler
+    public void onPing(ServerListPingEvent event) {
+        String s = Main.plugin.getConfig().getString("Server List Motd");
+        s = ChatColor.translateAlternateColorCodes('&', s);
+        event.setMotd(s);
+    }
+}

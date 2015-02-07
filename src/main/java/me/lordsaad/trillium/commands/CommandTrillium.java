d0e3375ed92f6c32368bdc0439eeac77274442c1
LordSaad <saadodi44@gmail.com>
2015-02-07 18:17:59 +0200

    support email

--- src/main/java/me/lordsaad/trillium/commands/CommandTrillium.java
+++ src/main/java/me/lordsaad/trillium/commands/CommandTrillium.java
@@ -11,23 +11,26 @@ public class CommandTrillium implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         if (cmd.getName().equalsIgnoreCase("trillium")) {
-            if (sender.hasPermission("tr.admin")) {
-                sender.sendMessage(ChatColor.DARK_GRAY + "<<<---{[O]}--->>> " + ChatColor.BLUE + "Trillium" + ChatColor.DARK_GRAY + " <<<---{[O]}--->>>");
-                sender.sendMessage(ChatColor.GRAY + "              Plugin made with love");
-                sender.sendMessage(ChatColor.GRAY + "       by LordSaad, VortexSeven, and TurboTailz");
-                sender.sendMessage(ChatColor.DARK_RED + "                          ❤");
-                sender.sendMessage(ChatColor.DARK_GRAY + "<<<-------------------------------->>>");
-                sender.sendMessage(ChatColor.GRAY + "Vesion: " + Main.plugin.getDescription().getVersion());
-                sender.sendMessage(ChatColor.GRAY + "Configuration Reloaded");
-                Main.plugin.reloadConfig();
-            } else {
-                sender.sendMessage(ChatColor.DARK_GRAY + "<<<---{[O]}--->>> " + ChatColor.BLUE + "Trillium" + ChatColor.DARK_GRAY + " <<<---{[O]}--->>>");
-                sender.sendMessage(ChatColor.GRAY + "              Plugin made with love");
-                sender.sendMessage(ChatColor.GRAY + "       by LordSaad44 and VortexSeven");
-                sender.sendMessage(ChatColor.DARK_RED + "                          ❤");
-                sender.sendMessage(ChatColor.DARK_GRAY + "<<<-------------------------------->>>");
-                sender.sendMessage(ChatColor.GRAY + "Vesion: " + Main.plugin.getDescription().getVersion());
-
+            if (args.length == 0) {
+                if (sender.hasPermission("tr.admin")) {
+                    sender.sendMessage(ChatColor.DARK_GRAY + "<<<---{[O]}--->>> " + ChatColor.BLUE + "Trillium" + ChatColor.DARK_GRAY + " <<<---{[O]}--->>>");
+                    sender.sendMessage(ChatColor.GRAY + "              Plugin made with love");
+                    sender.sendMessage(ChatColor.GRAY + "       by LordSaad, VortexSeven, and TurboTailz");
+                    sender.sendMessage(ChatColor.DARK_RED + "                          ❤");
+                    sender.sendMessage(ChatColor.DARK_GRAY + "<<<-------------------------------->>>");
+                    sender.sendMessage(ChatColor.GRAY + "Vesion: " + Main.plugin.getDescription().getVersion());
+                    sender.sendMessage(ChatColor.GRAY + "Configuration Reloaded");
+                    sender.sendMessage(ChatColor.GRAY + "Please report any bugs you find to: support@gettrillium.net");
+                    Main.plugin.reloadConfig();
+                } else {
+                    sender.sendMessage(ChatColor.DARK_GRAY + "<<<---{[O]}--->>> " + ChatColor.BLUE + "Trillium" + ChatColor.DARK_GRAY + " <<<---{[O]}--->>>");
+                    sender.sendMessage(ChatColor.GRAY + "              Plugin made with love");
+                    sender.sendMessage(ChatColor.GRAY + "       by LordSaad, VortexSeven, and TurboTailz");
+                    sender.sendMessage(ChatColor.DARK_RED + "                          ❤");
+                    sender.sendMessage(ChatColor.DARK_GRAY + "<<<-------------------------------->>>");
+                    sender.sendMessage(ChatColor.GRAY + "Vesion: " + Main.plugin.getDescription().getVersion());
+                    sender.sendMessage(ChatColor.GRAY + "Please report any bugs you find to: support@gettrillium.net");
+                }
             }
         }
         return true;

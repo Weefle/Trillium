dca60e301708bb6e0fc75c044089a3f37c81573f
LordSaad <saadodi44@gmail.com>
2015-02-08 13:52:51 +0200

    null check in info

--- src/main/java/me/lordsaad/trillium/commands/CommandInfo.java
+++ src/main/java/me/lordsaad/trillium/commands/CommandInfo.java
@@ -20,25 +20,29 @@ public class CommandInfo implements CommandExecutor {
                     Message.earg(sender, "Info", "/info <player>");
                 } else {
                     Player p = Bukkit.getPlayer(args[0]);
-                    p.sendMessage(" ");
-                    Message.m(MType.R, sender, "Info", "Displaying Information on: " + p.getName());
-                    Message.m(MType.R, sender, "Info", "Nickname: " + ChatColor.AQUA + API.getnickname(p));
-                    Message.m(MType.R, sender, "Info", "Online: " + ChatColor.AQUA + API.isonline(p));
-                    Message.m(MType.R, sender, "Info", "Gamemode: " + ChatColor.AQUA + API.getgamemode(p));
-                    Message.m(MType.R, sender, "Info", "Banned: " + ChatColor.AQUA +p.isBanned());
-                    if (p.isBanned()) {
-                        Message.m(MType.R, sender, "Info", "Ban Reason: 'You are the weakest link. Goodbye.'");
+                    if (p != null) {
+                        p.sendMessage(" ");
+                        Message.m(MType.R, sender, "Info", "Displaying Information on: " + p.getName());
+                        Message.m(MType.R, sender, "Info", "Nickname: " + ChatColor.AQUA + API.getnickname(p));
+                        Message.m(MType.R, sender, "Info", "Online: " + ChatColor.AQUA + API.isonline(p));
+                        Message.m(MType.R, sender, "Info", "Gamemode: " + ChatColor.AQUA + API.getgamemode(p));
+                        Message.m(MType.R, sender, "Info", "Banned: " + ChatColor.AQUA + p.isBanned());
+                        if (p.isBanned()) {
+                            Message.m(MType.R, sender, "Info", "Ban Reason: 'You are the weakest link. Goodbye.'");
+                        }
+                        Message.m(MType.R, sender, "Info", "Muted: " + ChatColor.AQUA + API.ismuted(p));
+                        Message.m(MType.R, sender, "Info", "Flying: " + ChatColor.AQUA + API.isflying(p));
+                        Message.m(MType.R, sender, "Info", "Location: " + ChatColor.AQUA + API.locationstring(p));
+                        if (!API.isonline(p)) {
+                            Message.m(MType.R, sender, "Info", "Last found at: " + ChatColor.AQUA + API.lastlocationstring(p));
+                        }
+                        Message.m(MType.R, sender, "Info", "Food level: " + ChatColor.AQUA + API.getfoodlevel(p));
+                        Message.m(MType.R, sender, "Info", "Health level: " + ChatColor.AQUA + API.gethealthlevel(p));
+                        Message.m(MType.R, sender, "Info", "Time Played: hours: " + ChatColor.AQUA + (API.gettimeplayed(p) / 60) / 60);
+                        Message.m(MType.R, sender, "Info", "Time Played: days: " + ChatColor.AQUA + ((API.gettimeplayed(p) / 60) / 60) / 24);
+                    } else {
+                        Message.eplayer(sender, "Info", args[0]);
                     }
-                    Message.m(MType.R, sender, "Info", "Muted: " + ChatColor.AQUA + API.ismuted(p));
-                    Message.m(MType.R, sender, "Info", "Flying: " + ChatColor.AQUA + API.isflying(p));
-                    Message.m(MType.R, sender, "Info", "Location: " + ChatColor.AQUA + API.locationstring(p));
-                    if (!API.isonline(p)) {
-                        Message.m(MType.R, sender, "Info", "Last found at: " + ChatColor.AQUA + API.lastlocationstring(p));
-                    }
-                    Message.m(MType.R, sender, "Info", "Food level: " + ChatColor.AQUA + API.getfoodlevel(p));
-                    Message.m(MType.R, sender, "Info", "Health level: " + ChatColor.AQUA + API.gethealthlevel(p));
-                    Message.m(MType.R, sender, "Info", "Time Played: hours: " + ChatColor.AQUA + (API.gettimeplayed(p) / 60) / 60);
-                    Message.m(MType.R, sender, "Info", "Time Played: days: " + ChatColor.AQUA + ((API.gettimeplayed(p) / 60) / 60) / 24);
                 }
             } else {
                 Message.e(sender, "Info", Crit.P);

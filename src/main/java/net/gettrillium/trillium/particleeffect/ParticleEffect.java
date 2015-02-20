fb110b81e50b9be047c75e31f353ab13ede75957
Henry Ammermann <hintsss@gmail.com>
2015-02-20 01:25:37 -0800

    I blame saad

--- src/main/java/net/gettrillium/trillium/particleeffect/ParticleEffect.java
+++ src/main/java/net/gettrillium/trillium/particleeffect/ParticleEffect.java
@@ -15,6 +15,30 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+/**
+ * <b>ParticleEffect Library</b>
+ * <p>
+ * This library was created by @DarkBlade12 and allows you to display all Minecraft particle effects on a Bukkit server
+ * <p>
+ * You are welcome to use it, modify it and redistribute it under the following conditions:
+ * <ul>
+ * <li>Don't claim this class as your own
+ * <li>Don't remove this disclaimer
+ * </ul>
+ * <p>
+ * Special thanks:
+ * <ul>
+ * <li>@microgeek (original idea, names and packet parameters)
+ * <li>@ShadyPotato (1.8 names, ids and packet parameters)
+ * <li>@RingOfStorms (particle behavior)
+ * <li>@Cybermaxke (particle behavior)
+ * </ul>
+ * <p>
+ * <i>It would be nice if you provide credit to me if you use this class in a published project</i>
+ *
+ * @author DarkBlade12
+ * @version 1.7
+ */
 public enum ParticleEffect {
     EXPLOSION_NORMAL("explode", 0, -1, ParticleProperty.DIRECTIONAL),
     EXPLOSION_LARGE("largeexplode", 1, -1),

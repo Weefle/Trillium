9ccd40a09f87b61ef5c899936cde1bb22197d2fa
Henry Ammermann <hintsss@gmail.com>
2015-02-20 00:59:05 -0800

    lowerCamelCase the var names

--- src/main/java/net/gettrillium/trillium/runnables/TpsRunnable.java
+++ src/main/java/net/gettrillium/trillium/runnables/TpsRunnable.java
@@ -2,27 +2,27 @@ package net.gettrillium.trillium.runnables;
 
 public class TpsRunnable implements Runnable {
 
-    public static int TickCount = 0;
-    public static long[] Ticks = new long[600];
+    public static int tickCount = 0;
+    public static long[] ticks = new long[600];
 
     public static double getTPS() {
         return getTPS(100);
     }
 
     public static double getTPS(int ticks) {
-        if (TickCount <= ticks) {
+        if (tickCount <= ticks) {
             return 20.0D;
         }
 
-        int target = (TickCount - 1 - ticks) % Ticks.length;
-        long elapsed = System.currentTimeMillis() - Ticks[target];
+        int target = (tickCount - 1 - ticks) % TpsRunnable.ticks.length;
+        long elapsed = System.currentTimeMillis() - TpsRunnable.ticks[target];
 
         return ticks / (elapsed / 1000.0D);
     }
 
     public void run() {
-        Ticks[(TickCount % Ticks.length)] = System.currentTimeMillis();
+        ticks[(tickCount % ticks.length)] = System.currentTimeMillis();
 
-        TickCount += 1;
+        tickCount += 1;
     }
 }

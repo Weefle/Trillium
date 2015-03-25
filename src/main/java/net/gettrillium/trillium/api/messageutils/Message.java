4ce5acb7eee18bdca0c4a514f474ba2aae8e6184
RedTheProgramerMan <redtheprogrammerman@gmail.com>
2015-03-25 16:03:57 -0500

    Added some cool one class messaging util type stuffs

--- src/main/java/net/gettrillium/trillium/api/messageutils/Message.java
+++ src/main/java/net/gettrillium/trillium/api/messageutils/Message.java
@@ -5,13 +5,29 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class Message {
+public enum Message {
 
-    public static void broadcast(Type type, String tag, String msg) {
+    WARNING(ChatColor.RED + ""),
+    GOOD(ChatColor.GREEN + ""),
+    GENERIC(ChatColor.BLUE + "");
+
+    private Message type;
+    private String prefix;
+
+    private Message(Message type) {
+        type = type;
+        prefix = type.prefix;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public void broadcast(String tag, String msg) {
         Bukkit.broadcastMessage(ChatColor.DARK_GRAY + "[" + type.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + msg);
     }
 
-    public static void message(Type type, CommandSender cs, String tag, boolean dontFlip, String msg) {
+    public void message(CommandSender cs, String tag, boolean dontFlip, String msg) {
         if (dontFlip) {
             cs.sendMessage(ChatColor.DARK_GRAY + "[" + type.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + msg);
         } else {
@@ -19,23 +35,23 @@ public class Message {
         }
     }
 
-    public static void error(String tag, CommandSender cs, String player) {
+    public void error(String tag, CommandSender cs, String player) {
         cs.sendMessage(ChatColor.DARK_GRAY + "[" + Type.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + player + " is either offline or does not exist.");
     }
 
-    public static void error(String tag, CommandSender cs) {
+    public void error(String tag, CommandSender cs) {
         if (!(cs instanceof Player)) {
-            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Type.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "You can't do that you silly console.");
+            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Message.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "You can't do that you silly console.");
         } else {
             cs.sendMessage(ChatColor.DARK_GRAY + "[" + Type.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "You don't have permission to do that.");
         }
     }
 
-    public static void error(CommandSender cs, String tag, boolean fewArgs, String usage) {
+    public void error(CommandSender cs, String tag, boolean fewArgs, String usage) {
         if (fewArgs) {
-            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Type.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "Too few arguments. " + usage);
+            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Message.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "Too few arguments. " + usage);
         } else {
-            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Type.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "Wrong arguments. " + usage);
+            cs.sendMessage(ChatColor.DARK_GRAY + "[" + Message.WARNING.getPrefix() + tag + ChatColor.DARK_GRAY + "]" + ChatColor.BLUE + " >> " + ChatColor.GRAY + "Wrong arguments. " + usage);
 
         }
     }

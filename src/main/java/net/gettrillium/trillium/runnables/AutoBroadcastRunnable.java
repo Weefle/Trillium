4ea89bb6295d04bb8f281d6977f5a31d9ddc2c44
Henry Ammermann <hintsss@gmail.com>
2015-07-30 01:03:33 -0700

    this *should* work (fixes to broadcastrunnable)i

--- src/main/java/net/gettrillium/trillium/runnables/AutoBroadcastRunnable.java
+++ src/main/java/net/gettrillium/trillium/runnables/AutoBroadcastRunnable.java
@@ -8,34 +8,42 @@ import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 
 public class AutoBroadcastRunnable extends BukkitRunnable {
-
     private int queue = 1;
+    private int min = Integer.MAX_VALUE;
+    private int max = 0;
 
-    public void run() {
-        SortedSet<Integer> set = new TreeSet<>();
-
+    public AutoBroadcastRunnable() {
         for (String i : TrilliumAPI.getInstance().getConfig().getConfigurationSection(Configuration.Broadcast.AUTO_BROADCASTS).getKeys(false)) {
-            if (StringUtils.isNumeric(i)) {
-                set.add(Integer.parseInt(i));
+            if (!StringUtils.isNumeric(i)) {
+                continue;
+            }
 
-                if (Integer.parseInt(i) == this.queue) {
-                    if (set.last() == this.queue) {
-                        this.queue = set.first();
-                    } else {
-                        this.queue++;
-                    }
-                }
+            int num = Integer.parseInt(i);
+
+            if (num > max) {
+                max = num;
+            } else if (num < min) {
+                min = num;
             }
         }
 
+        queue = min;
+    }
+
+    public void run() {
         List<String> list = TrilliumAPI.getInstance().getConfig().getStringList(Configuration.Broadcast.AUTO_BROADCASTS + "." + this.queue);
+
         for (String s : list) {
             s = ChatColor.translateAlternateColorCodes('&', s);
             Bukkit.broadcastMessage(s);
+        }
+
+        queue++;
+
+        if (queue > max) {
+            queue = min;
         }
     }
 }

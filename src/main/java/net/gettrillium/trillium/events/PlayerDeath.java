ac18873fb7386f6a41bde593d3139901c31c991a
Saad <saadodi44@gmail.com>
2015-07-30 17:19:09 +0300

    Added respawn screen cancelling back.

--- src/main/java/net/gettrillium/trillium/events/PlayerDeath.java
+++ src/main/java/net/gettrillium/trillium/events/PlayerDeath.java
@@ -13,7 +13,22 @@ public class PlayerDeath implements Listener {
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player p = event.getEntity();
         if (TrilliumAPI.getInstance().getConfig().getBoolean(Configuration.Ability.AUTO_RESPAWN)) {
-            p.spigot().respawn();
+            try {
+                Object nmsPlayer = p.getClass().getMethod("getHandle").invoke(p);
+                Object packet = Class.forName(nmsPlayer.getClass().getPackage().getName() + ".PacketPlayInClientCommand").newInstance();
+                Class<?> enumClass = Class.forName(nmsPlayer.getClass().getPackage().getName() + ".EnumClientCommand");
+
+                for (Object ob : enumClass.getEnumConstants()) {
+                    if (ob.toString().equals("PERFORM_RESPAWN")) {
+                        packet = packet.getClass().getConstructor(enumClass).newInstance(ob);
+                    }
+                }
+
+                Object con = nmsPlayer.getClass().getField("playerConnection").get(nmsPlayer);
+                con.getClass().getMethod("a", packet.getClass()).invoke(con, packet);
+            } catch (Throwable t) {
+                t.printStackTrace();
+            }
         }
 
         if (!TrilliumAPI.getInstance().getConfig().getBoolean(Configuration.Chat.ENABLE_DEATH_MESSAGES)) {

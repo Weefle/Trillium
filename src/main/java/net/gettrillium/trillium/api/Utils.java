57c5c964af84ae26cba4baf8dc43433a8149e1a0
Henry Ammermann <hintsss@gmail.com>
2015-08-03 04:05:32 -0700

    beef up the stringSplitter and write some tests for it

--- src/main/java/net/gettrillium/trillium/api/Utils.java
+++ src/main/java/net/gettrillium/trillium/api/Utils.java
@@ -90,10 +90,17 @@ public class Utils {
         }
     }
 
-    // TODO - unit test this, make more efficient
     // http://stackoverflow.com/a/12297231/4327834
     // #efficiency
     public static String[] stringSplitter(String s, int interval) {
+        if ((s == null) || (s.length() == 0)) {
+            return new String[] {};
+        }
+
+        if (interval == 0) {
+            return new String[] {s};
+        }
+
         int arrayLength = (int) Math.ceil((((double) s.length() / (double) interval)));
         String[] result = new String[arrayLength];
 
--- src/test/java/net/gettrillium/trillium/api/UtilsTest.java
+++ src/test/java/net/gettrillium/trillium/api/UtilsTest.java
@@ -1,24 +1,33 @@
 package net.gettrillium.trillium.api;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
 
 public class UtilsTest {
 
     @Test
+    public void stringSplitterTest() {
+        Assert.assertArrayEquals("null input string returns empty array", new String[] {}, Utils.stringSplitter(null, 1));
+        Assert.assertArrayEquals("blank input string returns empty array", new String[] {}, Utils.stringSplitter("", 1));
+        Assert.assertArrayEquals("interval of 0 returns an array with only the input string", new String[] {"abcde"}, Utils.stringSplitter("abcde", 0));
+        Assert.assertArrayEquals("interval of 1 vs 1 length string", new String[] {"a"}, Utils.stringSplitter("a", 1));
+        Assert.assertArrayEquals("interval of 1 vs 2 length string", new String[] {"a", "b"}, Utils.stringSplitter("ab", 1));
+        Assert.assertArrayEquals("interval of 2 vs 4 length string", new String[] {"ab", "cd"}, Utils.stringSplitter("abcd", 2));
+    }
+
+    @Test
     public void timeToTickConverterTest() {
-        assertEquals("blank goes to zero", 0, Utils.timeToTickConverter(""));
-        assertEquals("1 second goes to 20 ticks", 20, Utils.timeToTickConverter("1s"));
-        assertEquals("20 seconds goes to 400 ticks", 400, Utils.timeToTickConverter("20s"));
-        assertEquals("10 minutes goes to 12000 ticks", 12000, Utils.timeToTickConverter("10m"));
-        assertEquals("1m2s to 1240 ticks", 1240, Utils.timeToTickConverter("2s1m"));
+        Assert.assertEquals("blank goes to zero", 0, Utils.timeToTickConverter(""));
+        Assert.assertEquals("1 second goes to 20 ticks", 20, Utils.timeToTickConverter("1s"));
+        Assert.assertEquals("20 seconds goes to 400 ticks", 400, Utils.timeToTickConverter("20s"));
+        Assert.assertEquals("10 minutes goes to 12000 ticks", 12000, Utils.timeToTickConverter("10m"));
+        Assert.assertEquals("1m2s to 1240 ticks", 1240, Utils.timeToTickConverter("2s1m"));
     }
 
     @Test
     public void timeToStringTest() {
-        assertEquals("60 seconds", "00:00:01:00", Utils.timeToString(60 * 20));
-        assertEquals("65 seconds", "00:00:01:05", Utils.timeToString(65 * 20));
-        assertEquals("200 seconds", "00:00:03:20", Utils.timeToString(200 * 20));
+        Assert.assertEquals("60 seconds", "00:00:01:00", Utils.timeToString(60 * 20));
+        Assert.assertEquals("65 seconds", "00:00:01:05", Utils.timeToString(65 * 20));
+        Assert.assertEquals("200 seconds", "00:00:03:20", Utils.timeToString(200 * 20));
     }
 }

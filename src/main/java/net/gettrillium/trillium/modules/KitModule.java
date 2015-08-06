a6ef895dade731fc2d3a216dcfde1a28c10d09b0
Henry Ammermann <hintsss@gmail.com>
2015-08-06 16:12:16 -0700

    stuff

--- src/main/java/net/gettrillium/trillium/modules/KitModule.java
+++ src/main/java/net/gettrillium/trillium/modules/KitModule.java
@@ -1,6 +1,11 @@
 package net.gettrillium.trillium.modules;
 
-import net.gettrillium.trillium.api.*;
+import net.gettrillium.trillium.api.Configuration;
+import net.gettrillium.trillium.api.Kit;
+import net.gettrillium.trillium.api.Permission;
+import net.gettrillium.trillium.api.TrilliumAPI;
+import net.gettrillium.trillium.api.TrilliumModule;
+import net.gettrillium.trillium.api.TrilliumPlayer;
 import net.gettrillium.trillium.api.command.Command;
 import net.gettrillium.trillium.api.cooldown.Cooldown;
 import net.gettrillium.trillium.api.cooldown.CooldownType;
@@ -31,7 +36,9 @@ public class KitModule extends TrilliumModule {
             } else if (args.length == 1) {
                 if (getConfig().getConfigurationSection(Configuration.Kit.KIT_MAKER).contains(args[0])) {
                     if (p.getProxy().hasPermission(Permission.Kit.USE + args[0])) {
-                        if (!Cooldown.hasCooldown(p.getProxy(), CooldownType.KIT)) {
+                        if (Cooldown.hasCooldown(p.getProxy(), CooldownType.KIT)) {
+                            new Message(Mood.BAD, "Kit", "Cooldown is still active: " + ChatColor.AQUA + Cooldown.getTime(p.getProxy(), CooldownType.KIT)).to(p);
+                        } else {
                             if (!p.getProxy().isOp()
                                     && !p.hasPermission(Permission.Kit.COOLDOWN_EXEMPT)
                                     && !p.hasPermission(Permission.Kit.GIVE)) {
@@ -41,8 +48,6 @@ public class KitModule extends TrilliumModule {
                             new Kit(args[0]).giveTo(p.getProxy());
                             new Message(Mood.GOOD, TrilliumAPI.getName(cmd), "You successfully received kit " + args[0]).to(p);
 
-                        } else {
-                            new Message(Mood.BAD, TrilliumAPI.getName(cmd), "Cooldown is still active: " + ChatColor.AQUA + Cooldown.getTime(p.getProxy(), CooldownType.KIT)).to(p);
                         }
                     } else {
                         new Message(Mood.BAD, TrilliumAPI.getName(cmd), "You don't have permission to use that kit.").to(p);

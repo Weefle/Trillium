475bd8ae69a23fd2454b9474a78a003793d700ed
Henry Ammermann <hintsss@gmail.com>
2015-08-09 04:04:39 -0700

    fix up the message class

--- src/main/java/net/gettrillium/trillium/api/messageutils/Message.java
+++ src/main/java/net/gettrillium/trillium/api/messageutils/Message.java
@@ -7,19 +7,29 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import java.util.regex.Pattern;
+
 public class Message {
 
+    private static final Pattern PREFIX = Pattern.compile("%PREFIX%", Pattern.LITERAL);
+    private static final Pattern MESSAGE = Pattern.compile("%MESSAGE%", Pattern.LITERAL);
+    private static final Pattern MOOD_COLOR = Pattern.compile("%MOOD_COLOR%", Pattern.LITERAL);
+    private static final Pattern MAJOR = Pattern.compile("%MAJOR%", Pattern.LITERAL);
+    private static final Pattern MINOR = Pattern.compile("%MINOR%", Pattern.LITERAL);
+    private static final Pattern HILIGHT = Pattern.compile("%HIGHLIGHT%", Pattern.LITERAL);
+    private static final Pattern EXTRA = Pattern.compile("%EXTRA%", Pattern.LITERAL);
+    private static final Pattern USERNAME = Pattern.compile("%USERNAME%", Pattern.LITERAL);
     private String format;
 
     public Message(Mood mood, String prefix, String message) {
         format = TrilliumAPI.getInstance().getConfig().getString(PluginMessages.FORMAT);
         format = ChatColor.translateAlternateColorCodes('&', format);
-        format = format.replace("%PREFIX%", prefix);
-        format = format.replace("%MESSAGE%", message);
-        format = format.replace("%MOOD_COLOR%", mood.getColor());
-        format = format.replace("%MAJOR%", Pallete.getMajor() + "");
-        format = format.replace("%MINOR%", Pallete.getMinor() + "");
-        format = format.replace("%HIGHLIGHT%", Pallete.getHighlight() + "");
+        format = PREFIX.matcher(format).replaceAll(prefix);
+        format = MESSAGE.matcher(format).replaceAll(message);
+        format = MOOD_COLOR.matcher(format).replaceAll(mood.getColor());
+        format = MAJOR.matcher(format).replaceAll(Pallete.getMajor() + "");
+        format = MINOR.matcher(format).replaceAll(Pallete.getMinor() + "");
+        format = HILIGHT.matcher(format).replaceAll(Pallete.getHighlight() + "");
         format = ChatColor.translateAlternateColorCodes('&', format);
     }
 
@@ -30,13 +40,17 @@ public class Message {
 
     public Message(String prefix, Error error, String extra) {
         this(Mood.BAD, prefix, error.getError());
-        format = format.replace("%EXTRA%", extra);
-        format = format.replace("%USERNAME%", extra);
+        format = EXTRA.matcher(format).replaceAll(extra);
+        format = USERNAME.matcher(format).replaceAll(extra);
         format = ChatColor.translateAlternateColorCodes('&', format);
     }
 
     public void broadcast() {
         Bukkit.broadcastMessage(format);
+    }
+
+    public void broadcast(String perm) {
+        Bukkit.broadcast(format, perm);
     }
 
     public void to(CommandSender to) {

9077540f49f04e66a30ad6946cac3c7ff5e7e80e
Henry Ammermann <hintsss@gmail.com>
2015-08-18 19:51:57 -0700

    daily commit

--- src/main/java/net/gettrillium/trillium/api/messageutils/Mood.java
+++ src/main/java/net/gettrillium/trillium/api/messageutils/Mood.java
@@ -1,15 +1,15 @@
 package net.gettrillium.trillium.api.messageutils;
 
-import net.gettrillium.trillium.api.Configuration;
+import net.gettrillium.trillium.api.Configuration.PluginMessages;
 import net.gettrillium.trillium.api.TrilliumAPI;
 import org.bukkit.ChatColor;
 
 public enum Mood {
-    BAD(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(Configuration.PluginMessages.BAD_COLOR))),
-    GOOD(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(Configuration.PluginMessages.GOOD_COLOR))),
-    NEUTRAL(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(Configuration.PluginMessages.NEUTRAL_COLOR)));
+    BAD(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(PluginMessages.BAD_COLOR))),
+    GOOD(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(PluginMessages.GOOD_COLOR))),
+    NEUTRAL(ChatColor.translateAlternateColorCodes('&', TrilliumAPI.getInstance().getConfig().getString(PluginMessages.NEUTRAL_COLOR)));
 
-    private String color;
+    private final String color;
 
     Mood(String color) {
         this.color = color;

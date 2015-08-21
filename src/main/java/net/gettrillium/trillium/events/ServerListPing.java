01353a0e0dcd7ace7f41cd0cb2c38635cd4c26d1
Henry Ammermann <hintsss@gmail.com>
2015-08-21 08:24:22 -0700

    use precompiled patterns

--- src/main/java/net/gettrillium/trillium/events/ServerListPing.java
+++ src/main/java/net/gettrillium/trillium/events/ServerListPing.java
@@ -7,14 +7,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
+import java.util.regex.Pattern;
+
 public class ServerListPing implements Listener {
+
+    private static final Pattern ONLINE = Pattern.compile("%ONLINEPLAYERS%", Pattern.LITERAL);
+    private static final Pattern MAX = Pattern.compile("%MAXPLAYERS%", Pattern.LITERAL);
 
     @EventHandler
     public void onPing(ServerListPingEvent event) {
         String motd = TrilliumAPI.getInstance().getConfig().getString(Configuration.Chat.SERVER_LIST_MOTD);
         motd = ChatColor.translateAlternateColorCodes('&', motd);
-        motd = motd.replace("%ONLINEPLAYERS%", String.valueOf(event.getNumPlayers()));
-        motd = motd.replace("%MAXPLAYERS%", String.valueOf(event.getMaxPlayers()));
+        motd = ONLINE.matcher(motd).replaceAll(String.valueOf(event.getNumPlayers()));
+        motd = MAX.matcher(motd).replaceAll(String.valueOf(event.getMaxPlayers()));
         event.setMotd(motd);
     }
 }

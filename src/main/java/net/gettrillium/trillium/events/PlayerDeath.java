62f81b0aa781e661075b5ba25ef7799fef76e647
Henry Ammermann <hintsss@gmail.com>
2015-08-23 01:47:58 -0700

    daily commit

--- src/main/java/net/gettrillium/trillium/events/PlayerDeath.java
+++ src/main/java/net/gettrillium/trillium/events/PlayerDeath.java
@@ -1,18 +1,26 @@
 package net.gettrillium.trillium.events;
 
-import net.gettrillium.trillium.api.Configuration;
+import net.gettrillium.trillium.api.Configuration.Ability;
+import net.gettrillium.trillium.api.Configuration.Chat;
 import net.gettrillium.trillium.api.TrilliumAPI;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class PlayerDeath implements Listener {
 
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         Player p = event.getEntity();
-        if (TrilliumAPI.getInstance().getConfig().getBoolean(Configuration.Ability.AUTO_RESPAWN)) {
+
+        if (!TrilliumAPI.getInstance().getConfig().getBoolean(Chat.ENABLE_DEATH_MESSAGES)) {
+            event.setDeathMessage(null);
+        }
+
+        if (TrilliumAPI.getInstance().getConfig().getBoolean(Ability.AUTO_RESPAWN)) {
             try {
                 Object nmsPlayer = p.getClass().getMethod("getHandle").invoke(p);
                 Object packet = Class.forName(nmsPlayer.getClass().getPackage().getName() + ".PacketPlayInClientCommand").newInstance();
@@ -26,13 +34,9 @@ public class PlayerDeath implements Listener {
 
                 Object con = nmsPlayer.getClass().getField("playerConnection").get(nmsPlayer);
                 con.getClass().getMethod("a", packet.getClass()).invoke(con, packet);
-            } catch (Throwable t) {
-                t.printStackTrace();
+            } catch (SecurityException | IllegalAccessException | ClassNotFoundException | InvocationTargetException | NoSuchFieldException | InstantiationException | NoSuchMethodException e) {
+                e.printStackTrace();
             }
-        }
-
-        if (!TrilliumAPI.getInstance().getConfig().getBoolean(Configuration.Chat.ENABLE_DEATH_MESSAGES)) {
-            event.setDeathMessage(null);
         }
     }
 }
